2b733974169177adb9c4a7cd578112f3c43a333f
craftzcode <tomymaritano@gmail.com>
2024-03-08 17:30:54 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
-
+import { Box } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavbarMenu from "./components/Navbar/Navbar";
 import MainSide from "./components/Main/MainSide";
 import CVForm from "./components/CVForm/CVform";
 import Footer from "./components/Footer/Footer";
 import ParticlesJS from "./components/Particles/Particles";
-import Test from "./components/test";
 function App() {
   return (
     <>
       <ChakraProvider>
-        <ParticlesJS />
-        <NavbarMenu />
-        <MainSide />
-        <CVForm />
-        <Footer />
-        <Test />
+        <Box bgColor="#171717">
+          <ParticlesJS />
+          <NavbarMenu />
+          <MainSide />
+          <CVForm />
+          <Footer />
+        </Box>
       </ChakraProvider>
     </>
   );
